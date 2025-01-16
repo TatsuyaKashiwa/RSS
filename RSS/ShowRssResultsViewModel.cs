b1bb872f392b393e7346ae6c940e3bf44190b1a5
Tatsuya Kashiwa <sachikaze417@gmail.com>
2025-01-16 16:19:05 +0900

    feat : 各プロパティに対応する変数作成

--- RSS/ShowRssResultsViewModel.cs
+++ RSS/ShowRssResultsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reactive.Bindings;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -10,5 +11,13 @@ namespace RSS
     class ShowRssResultsViewModel
     {
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ReactivePropertySlim<string> Url {  get; set; }
+
+        public ReactivePropertySlim<string> SearchWord { get; set; }
+
+        public ReactivePropertySlim<string> RssValue {  get; set; }
+
+        public ReactiveCommandSlim ShowRssCommand { get; }
     }
 }
